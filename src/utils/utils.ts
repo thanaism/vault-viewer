b64f424bbe63ff3eb6df592fea075c0f99c7f786
thanai-mac <62784012+thanaism@users.noreply.github.com>
2022-11-03 13:28:39 +0900

    add: コントラクト戻り値の余計な配列をunwrapするユーティリティ関数の追加

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,23 +1,29 @@
 import { ethers } from 'ethers';
 
 export const toName = (chainId: string) => {
-    switch (Number(chainId)) {
-        case 1:
-            return 'mainnet';
-        case 4:
-            return 'rinkeby';
-        case 5:
-            return 'goerli';
-        case 137:
-            return 'polygon';
-        default:
-            return 'unknown';
-    }
+  switch (Number(chainId)) {
+    case 1:
+      return 'mainnet';
+    case 4:
+      return 'rinkeby';
+    case 5:
+      return 'goerli';
+    case 137:
+      return 'polygon';
+    default:
+      return 'unknown';
+  }
 };
 
 type ResponseBigNumber = { type: 'BigNumber'; hex: string }[];
 
 export const BN = (input: ResponseBigNumber | string | number) =>
-    ethers.BigNumber.from(Array.isArray(input) ? input[0] : input);
+  ethers.BigNumber.from(Array.isArray(input) ? input[0] : input);
 
 export const durationToDays = (duration: ResponseBigNumber) => BN(duration).div(BN(86400)).toString();
+
+export const unwrapParenthesesIfSingleElement = (input: unknown) => {
+  if (!Array.isArray(input)) throw Error('input is not an array');
+  if (input.length !== 1) throw Error('element is not single');
+  return input[0];
+};
